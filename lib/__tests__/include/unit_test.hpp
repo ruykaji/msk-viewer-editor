da2bd5613e0006224edddfb59cf792f6574ccc45
Ruykaji <ilya.tanc@gmail.com>
2023-09-13 13:53:38 +0300

    Update unit_test.hpp

--- lib/__tests__/include/unit_test.hpp
+++ lib/__tests__/include/unit_test.hpp
@@ -7,18 +7,17 @@
 #ifndef SIMPLE_UNIT_TEST_HPP
 #define SIMPLE_UNIT_TEST_HPP
 
-class TestFramework
-{
+class TestFramework {
     using TestCaseFunc = std::function<void()>;
 
-    std::vector<std::pair<std::string, TestCaseFunc>> m_testCases{};
-    std::vector<std::pair<std::string, std::pair<uint64_t, uint64_t>>> m_casesAssertions{};
-    uint64_t m_passedAssertions{};
-    uint64_t m_failedAssertions{};
-    bool m_isPassedAssertion{};
+    std::vector<std::pair<std::string, TestCaseFunc>> m_testCases {};
+    std::vector<std::pair<std::string, std::pair<uint64_t, uint64_t>>> m_casesAssertions {};
+    uint64_t m_passedAssertions {};
+    uint64_t m_failedAssertions {};
+    bool m_isPassedAssertion {};
 
-  public:
-    static TestFramework &getInstance()
+public:
+    static TestFramework& getInstance()
     {
         static TestFramework instance;
         return instance;
@@ -35,7 +34,7 @@ class TestFramework
         ++m_failedAssertions;
     }
 
-    void registerTestCase(const std::string &t_testDescription, const TestCaseFunc &t_testFunc)
+    void registerTestCase(const std::string& t_testDescription, const TestCaseFunc& t_testFunc)
     {
         m_casesAssertions.emplace_back(t_testDescription, std::make_pair(m_passedAssertions, m_failedAssertions));
         m_testCases.emplace_back(t_testDescription, t_testFunc);
@@ -43,75 +42,73 @@ class TestFramework
 
     void runTests()
     {
-        std::vector<std::pair<std::string, bool>> testResults{};
+        std::vector<std::pair<std::string, bool>> testResults {};
         uint64_t totalCases = m_testCases.size();
-        uint64_t failedCases{};
-        uint64_t totalPassedAssertions{};
-        uint64_t totalFailedAssertions{};
+        uint64_t failedCases {};
+        uint64_t totalPassedAssertions {};
+        uint64_t totalFailedAssertions {};
 
-        std::cout << "\nSimple Unit Testing\n\n";
+        std::cout << "\033[34m\nSimple Unit Testing\n\n";
         std::cout << "====================================================================\n\n";
 
-        for (const auto &testCase : m_testCases)
-        {
-            try
-            {
+        for (const auto& testCase : m_testCases) {
+            try {
                 m_passedAssertions = 0;
                 m_failedAssertions = 0;
                 m_isPassedAssertion = true;
 
                 testCase.second();
 
-                if (m_isPassedAssertion)
-                {
+                if (m_isPassedAssertion) {
                     testResults.emplace_back(testCase.first, true);
-                }
-                else
-                {
+                } else {
                     testResults.emplace_back(testCase.first, false);
                     ++failedCases;
                 }
 
                 totalPassedAssertions += m_passedAssertions;
                 totalFailedAssertions += m_failedAssertions;
-            }
-            catch (const std::exception &e)
-            {
+            } catch (const std::exception& e) {
                 testResults.emplace_back(testCase.first, false);
                 ++failedCases;
                 ++totalFailedAssertions;
 
-                std::cout << "Unhandled exception: " << e.what() << "\n" << std::flush;
+                std::cout << "Unhandled exception: " << e.what() << "\n"
+                          << std::flush;
             }
         }
 
-        for (const auto &testResult : testResults)
-        {
-            if (testResult.second)
-            {
-                std::cout << "-- [  PASSED  ] -- " << testResult.first << "\n";
-            }
-            else
-            {
-                std::cout << "-- [  FAILED  ] -- " << testResult.first << "\n";
+        for (const auto& testResult : testResults) {
+            if (testResult.second) {
+                std::cout << "\033[32m-- [  PASSED  ] -- " << testResult.first << "\n";
+            } else {
+                std::cout << "\033[31m-- [  FAILED  ] -- " << testResult.first << "\n";
             }
         }
 
-        std::cout << "\n====================================================================\n";
-        std::cout << "Total tests " << totalCases << " : " << totalCases - failedCases << " passed | " << failedCases
+        std::cout << "\033[34m\n====================================================================\n";
+        std::cout << "Total tests "
+                  << totalCases << " : \033[32m"
+                  << totalCases - failedCases
+                  << " passed \033[32m|\033[31m "
+                  << failedCases
                   << " failed\n";
-        std::cout << "Total assertions " << totalFailedAssertions + totalPassedAssertions << " : "
-                  << totalPassedAssertions << " : passed | " << totalFailedAssertions << " failed\n\n"
+        std::cout << "\033[34mTotal assertions "
+                  << totalFailedAssertions + totalPassedAssertions
+                  << " : \033[32m"
+                  << totalPassedAssertions
+                  << " : passed \033[32m|\033[31m "
+                  << totalFailedAssertions
+                  << " failed\n\n\033[0m"
                   << std::flush;
     }
 };
 
-class TestUnit
-{
+class TestUnit {
     using TestCaseFunc = std::function<void()>;
 
-  public:
-    TestUnit(const std::string &t_testDescription, const TestCaseFunc &t_testFunc)
+public:
+    TestUnit(const std::string& t_testDescription, const TestCaseFunc& t_testFunc)
     {
         TestFramework::getInstance().registerTestCase(t_testDescription, t_testFunc);
     };
@@ -121,18 +118,18 @@ class TestUnit
 #define INTERNAL_UNIQUE_NAME_LINE(t_name, t_line) INTERNAL_UNIQUE_NAME_LINE2(t_name, t_line)
 #define INTERNAL_UNIQUE_NAME(t_name) INTERNAL_UNIQUE_NAME_LINE(t_name, __COUNTER__)
 
-#define INTERNAL_TEST_CASE2(t_testName, t_testDescription)                                                             \
-    void t_testName();                                                                                                 \
-    TestUnit INTERNAL_UNIQUE_NAME(autoRegister)(t_testDescription, t_testName);                                        \
+#define INTERNAL_TEST_CASE2(t_testName, t_testDescription)                      \
+    void t_testName();                                                          \
+    TestUnit INTERNAL_UNIQUE_NAME(autoRegister)(t_testDescription, t_testName); \
     void t_testName()
 #define INTERNAL_TEST_CASE(...) INTERNAL_TEST_CASE2(INTERNAL_UNIQUE_NAME(t_testName), __VA_ARGS__)
 
 #define TEST_CASE(...) INTERNAL_TEST_CASE(__VA_ARGS__)
 
-#define ASSERT(condition)                                                                                              \
-    if ((condition))                                                                                                   \
-        TestFramework::getInstance().updatePassedAssertions();                                                         \
-    else                                                                                                               \
+#define ASSERT(condition)                                      \
+    if ((condition))                                           \
+        TestFramework::getInstance().updatePassedAssertions(); \
+    else                                                       \
         TestFramework::getInstance().updateFailedAssertions();
 
 int main(void)
